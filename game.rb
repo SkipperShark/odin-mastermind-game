4af9a48f27d350593e80f3de6e4e4fe6ef0303ad
sharman <sharmanfaridz@gmail.com>
2024-05-07 19:48:12 +0800

    add secret code to board

--- game.rb
+++ game.rb
@@ -5,7 +5,10 @@ class Board
   attr_accessor :board
 
   def initialize
-    @board = Array.new(12) { [1, 2, 3, 4] }
+    @board = []
+    create_code_row
+    create_decode_rows
+    # @board = Array.new(12) { [1, 2, 3, 4] }
   end
 
   def show
@@ -13,26 +16,33 @@ class Board
       pp row
     end
   end
-end
 
+  def create_code_row
+    @board.prepend ['?', '?', '?', '?']
+  end
+
+  def create_decode_rows
+    @board += Array.new(12) { [1, 2, 3, 4] }
+  end
+end
 
 # represents a code peg that can be placed on the board, a code peg can be of various colours
 class CodePeg
   @@color_options = %w[red green purple yellow brown orange black white]
+  attr_reader :color
 
   def initialize(color_option)
     unless @@color_options.include?(color_option)
       puts 'that is not a valid colour option!'
       return
     end
-    @color_option = color_option
+    @color = color_option
   end
 
   def self.get_random_color
     @@color_options.sample
   end
 end
-
 
 class Game
   def initialize
@@ -41,29 +51,27 @@ class Game
     @board = Board.new
     introduction
     # @board.show
-    pp @human.code
-    pp @computer.code
+
+    puts "secret code : #{@computer.debug_get_code}"
   end
 
   def introduction
-    puts "Welcome to mastermind.\n"
+    puts "Welcome to mastermind\nThis is the board\n\n"
+    @board.show
+    print "\n"
   end
-
 end
 
-
 class Player
-
   attr_reader :code
 
-  #4 cases
+  # 4 cases
   # human codebreaker (do nothing)
   # human codemaker (ask for pattern)
   # computer codebreaker (do nothing)
   # computer codemaker (generate pattern)
 
   def initialize(is_codemaker:, is_human:)
-
     @code = nil unless is_codemaker
 
     if is_human
@@ -71,19 +79,21 @@ class Player
     else
       get_random_secret_pattern
     end
-
   end
 
   def prompt_secret_pattern
-    #todo implementation pending
+    # TODO: implementation pending
   end
 
   def get_random_secret_pattern
-    @code = Array.new(4) { CodePeg.new(CodePeg.get_random_color)}
+    @code = Array.new(4) { CodePeg.new(CodePeg.get_random_color) }
+  end
+
+  def debug_get_code
+    @code.map(&:color)
   end
 
   private
 
   attr_writer :code
-
 end
