d1556740b2b17fd1f71f5970e1f2553ab8ff228f
skipperShark <sharmanfaridz@gmail.com>
2024-06-28 15:14:28 +0800

    refactor key_peg according to rubocop

--- lib/key_peg.rb
+++ lib/key_peg.rb
@@ -1,9 +1,7 @@
-# frozen_string_literal: true
-
 # represents a key peg that can be placed on the feedback portion of the board
 class KeyPeg
-  FULL_MATCH_COLOR = "red"
-  POSITION_MATCH_COLOR = "white"
+  FULL_MATCH_COLOR = "red".freeze
+  POSITION_MATCH_COLOR = "white".freeze
   COLOR_OPTIONS = [FULL_MATCH_COLOR, POSITION_MATCH_COLOR].freeze
 
   private_class_method :new
@@ -14,11 +12,11 @@ class KeyPeg
   end
 
   def self.full_match
-    self.new(FULL_MATCH_COLOR)
+    new(FULL_MATCH_COLOR)
   end
 
   def self.position_match
-    self.new(POSITION_MATCH_COLOR)
+    new(POSITION_MATCH_COLOR)
   end
 
   def to_s
@@ -26,11 +24,7 @@ class KeyPeg
   end
 
   def full_match?
-    if color == FULL_MATCH_COLOR
-      true
-    else
-      false
-    end
+    color == FULL_MATCH_COLOR
   end
 
   def self.random_color
@@ -38,10 +32,6 @@ class KeyPeg
   end
 
   def position_match?
-    if color == POSITION_MATCH_COLOR
-      true
-    else
-      false
-    end
+    color == POSITION_MATCH_COLOR
   end
 end
