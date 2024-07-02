c37f03f7f3f40a706111c431c0f12e8cce65cf5f
sharman <sharmanfaridz@gmail.com>
2024-07-02 20:54:46 +0800

    refactor method to get user confirmation to utilities module

--- lib/utilites.rb
+++ lib/utilites.rb
@@ -3,4 +3,17 @@ module Utilities
   def user_input
     gets.chomp.downcase.strip
   end
+
+  def user_confirmed?(message = "Would you like to confirm? (y/n)")
+    valid_choice = false
+    until valid_choice == true
+      puts message
+      input = user_input
+      case input
+      when "y" then return true
+      when "n" then return false
+      else puts "I'm not sure what you mean, please try again"
+      end
+    end
+  end
 end
